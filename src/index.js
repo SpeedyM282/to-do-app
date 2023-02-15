2f51e0b691d74cf4f30271ac0d9e62752addb48c
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-15 19:14:42 +0500

    fix react app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+reportWebVitals();
