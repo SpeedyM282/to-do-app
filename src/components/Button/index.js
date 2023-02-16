9bdc7a5a6b33767a8fd3f311f3d15f369ef21587
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-16 19:02:26 +0500

    add button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './style.scss';
 
 function Button({ txt }) {
+  const style = {
+    backgroundColor: txt === 'Delete' ? '#EB2E2E' : '#007bff'
+  };
+
   return (
     <>
       <button
         className='button'
+        style={style}
       >
         {txt}
       </button>
