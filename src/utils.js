2522fc23291befa73a5f1666b0303dd9d5bbb400
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-24 15:00:08 +0500

    add utils

---
+++ src/utils.js
@@ -0,0 +1,23 @@
+import Todo from "./components/Todo";
+
+export function todoGenerator(id, title, description) {
+  return <Todo key={id} id={id} title={title} description={description} />;
+}
+
+export function todoDeleter(todos, id) {
+  return todos.filter(e => e.props.id !== id);
+}
+
+export function todoUpdater(todos, id, title, description) {
+  return todos.map(e => {
+    if (e.props.id === id) {
+      e.props.title = title;
+      e.props.description = description;
+    }
+    return e;
+  });
+}
+
+export function todosAssigner(data) {
+  return data.map(e => todoGenerator(e.id, e.title, e.description));
+}
