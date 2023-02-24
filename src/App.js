c7cdb1e522bbfb0f39c893e7b2b896bedec24487
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-24 15:00:00 +0500

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
-// import TodoList from './pages/TodoList';
+import TodoList from './pages/TodoList';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>APP File</h1> */}
-      <Login />
-      {/* <TodoList /> */}
+      <Routes>
+        <Route path='/to-do-app/' element={<Login />} />
+        <Route path='/to-do-app/user' element={<TodoList />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
 
 reportWebVitals();
