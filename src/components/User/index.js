c7ff74458217cc14418569baaea0ff7b54498f9c
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-25 22:55:25 +0500

    add User component

---
+++ src/components/User/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+function User({ login, role, name }) {
+  return (
+    <div className='user__block' >
+      <p className='user__block--text' >{login}</p>
+      <p className='user__block--text' >{role}</p>
+      <p className='user__block--text' >{name}</p>
+    </div>
+  );
+}
+
+export default User;
