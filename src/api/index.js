c89da2d4f9d31efd35f1a3f076e607cac54b8a54
speedym282 <abdulaxadrizakulov@gmail.com>
2023-02-25 22:53:26 +0500

    add api functionality for login/logout/me

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const client = axios.create({
+  withCredentials: true,
+  baseURL: 'http://localhost:3000/api/v1'
+});
+
+export function loginPOST(username, password) {
+  const data = {
+    "login": username,
+    "password": password
+  };
+
+  return client.post('/login', data);
+}
+
+export function logoutPOST() {
+  return client.post('/logout');
+}
+
+export function meGET() {
+  return client.get('/me');
+}
